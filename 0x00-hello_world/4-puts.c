540d98d503343e82d45fca1514a6fc96c502ddaf
Maryaamerhh <salisumaryam48@gmail.com>
2022-10-13 15:28:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
+
 #include <stdio.h>
 
-/** 
+/**
  * main - printing
  *
  * Return: Always 0 (sucessful)
@@ -8,5 +9,6 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
+ 
