1642d9ac68ea4ceea860382cf4ef530b77e8491c
Maryaamerhh <salisumaryam48@gmail.com>
2022-10-14 16:51:49 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing hexadecimal in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit;
+	char alphabet;
+
+	for (digit = 0; digit < 10; digit++)
+		putchar((digit % 10) + '0');
+
+	for (alphabet = 'a' ; alphabet <= 'f' ; alphabet++)
+		putchar(alphabet);
+
+	return (0);
+}
