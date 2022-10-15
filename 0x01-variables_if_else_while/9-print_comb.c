ef08170292e6a22ca0c5ca47fd44db6ef655fdc7
Maryaamerhh <salisumaryam48@gmail.com>
2022-10-14 17:12:43 -0700

    single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -printing combinations of single digit numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar((digit % 10) + '0');
+		if (digit == 9)
+			continue;
+		putchar(',');
+		putchar(',');
+	}
+	putchar('\n');
+
+	return (0);
+}
+
