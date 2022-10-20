8f5525383061db817c4d09f008e3e2bd11dc9cbb
Maryaamerhh <salisumaryam48@gmail.com>
2022-10-20 05:50:00 -0700

    0 to 14 x 10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,14 +3,14 @@
 /**
  * more_numbers - a function that prints 10 times the numbers
  * from 0 to 14
- *
+ * _putchar only 3 times
  * Return: from 0 to 14 x 10 followed by a new line.
  */
 void more_numbers(void)
 {
 	int m, me;
 
-                for (me = 0; me < 10; me++)
+	for (me = 0; me < 10; me++)
 
 	{
 		for (m = 0; m >= 14; m++)
