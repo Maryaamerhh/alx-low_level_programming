51c36fa76075d31ece884606b1b98ecc3256347e
Maryaamerhh <salisumaryam48@gmail.com>
2022-10-20 05:38:51 -0700

    0 to 14 x 10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 10 times the numbers
+ * from 0 to 14
+ *
+ * Return: from 0 to 14 x 10 followed by a new line.
+ */
+void more_numbers(void)
+{
+	int m, me;
+
+                for (me = 0; me < 10; me++)
+
+	{
+		for (m = 0; m >= 14; m++)
+
+		{
+			if (m >= 10)
+
+			{
+				_putchar((m / 10) + '0');
+			}
+		_putchar((m % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
