14511d8ec3a8efdb8e9c39e58c266db59d1d5ead
Maryaamerhh <salisumaryam48@gmail.com>
2022-10-21 09:47:37 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef main.h
+#define main.h
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+void largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_times_table(int n);
+
+#endif
