83d27e283caf1e87c6933de06097b007155e0fb5
Maryaamerhh <salisumaryam48@gmail.com>
2022-10-27 02:06:13 -0700

    concatenated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a, b;
+	int a = 0;
+	int b = 0;
 
-	for (a = 0; dest[a] != '\0'; a++)
-	{
-	}
-		for (b = 0; src[a + b] = *src++ != '\0'; b++)
-		{
-		}
-
-		return (dest);
+	while (dest[a++])
+		b++;
+	for (a = 0; src[a]; a++)
+		dest[b++] = src[a];
+	return (dest);
 }
-
 
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: concatenate to
+ * @src: concatenate from
+ * @n: input o
