5fa8fea845db18d9fabf59e5f20f176439a615bf
Maryaamerhh <salisumaryam48@gmail.com>
2022-10-31 20:33:33 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,18 +1,31 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - this function checks the code for ALX School students.
+ * main - this function gets the length of a prefix substring.
  *
- * Return: Always 0.
+ * Return: bytes
  */
-int main(void)
+unsigned int _strspn(char *s, char *accept)
 {
-char *s = "hello, world";
-char *f = "oleh";
-unsigned int n;
+unsigned int bytes = 0;
+	int index;
 
-n = _strspn(s, f);
-printf("%u\n", n);
-return (0);
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
 }
