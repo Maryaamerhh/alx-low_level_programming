0cc9e1bf1b97b342fb2789d300835b1c90bbd815
Maryaamerhh <salisumaryam48@gmail.com>
2022-11-02 19:50:35 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	int len = 0;
 
 	if (*s)
 	{
-		i++;
-		i += _strlen_recursion(s + 1);
+		len++;
+		len += _strlen_recursion(s + 1);
 	}
 
-	return (i);
+	return (len);
 }
