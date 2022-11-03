0d49755dc665a71b9204b47adfcfa171327aa4ea
Maryaamerhh <salisumaryam48@gmail.com>
2022-11-02 20:25:28 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,8 +3,6 @@
 /**
  * _print_rev - this function prints a string in reverse.
  * @s: The string to be returned
- *
- * Return: The length of the string.
  */
 void _print_rev_recursion(char *s)
 {
